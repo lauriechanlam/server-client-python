3ac1a7f02dd576f30bf65d17f68afcdb5e0be400
Laurie Chan Lam <67828166+lauriechanlam@users.noreply.github.com>
2020-11-27 14:16:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 # Only install pytest and runner when test command is run
